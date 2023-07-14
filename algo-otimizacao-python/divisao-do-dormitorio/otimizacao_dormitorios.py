79c2477a166c475f5b63642a48b88c79dded2df6
diogomarin <70486076+diogomarin@users.noreply.github.com>
2023-07-14 16:38:49 -0300

    implementacao do algoritmo genetico

--- algo-otimizacao-python/divisao-do-dormitorio/otimizacao_dormitorios.py
+++ algo-otimizacao-python/divisao-do-dormitorio/otimizacao_dormitorios.py
@@ -133,3 +133,60 @@ print('------------------------')
 print('SOLUCAO TEMPERA SIMULADA')
 print('------------------------')
 imprimir_solucao(solucao_tempera_simulada)
+
+def mutacao(dominio, passo, solucao):
+    i = random.randint(0, len(dominio) - 1)
+    mutante = solucao
+    
+    if random.random() < 0.5:
+        if solucao[i] != dominio[i][0]:
+            mutante = solucao[0:i] + [solucao[i] - passo] + solucao[i + 1:]
+    else:
+        if solucao[i] != dominio[i][1]:
+            mutante = solucao[0:i] + [solucao[i] + passo] + solucao[i + 1:]
+    
+    return mutante
+
+#s = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
+#s1 = mutacao(dominio, 1, s)
+
+def cruzamento(dominio, solucao1, solucao2):
+    i = random.randint(1, len(dominio)- 2)
+    return solucao1[0:i] + solucao2[i:]
+
+#s1 = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
+#s2 = [0, 1, 2, 5, 8, 9, 2, 3, 5, 1, 0, 6]
+#s3 = cruzamento(dominio, s1, s2)
+
+def genetico(dominio, funcao_custo, tamanho_populacao = 50, passo = 1, probabilidade_mutacao = 0.2, elitismo = 0.2, numero_geracoes = 100):
+    populacao = []
+    for i in range(tamanho_populacao):
+        solucao = [random.randint(dominio[i][0], dominio[i][1]) for i in range(len(dominio))]
+        populacao.append(solucao)
+        
+    numero_elitismo = int(elitismo * tamanho_populacao)
+    
+    for i in range(numero_geracoes):
+        custos = [(funcao_custo(individuo), individuo) for individuo in populacao]
+        custos.sort()
+        individuos_ordenados = [individuo for (custo, individuo) in custos]
+        
+        populacao = individuos_ordenados[0:numero_elitismo]
+        
+        while len(populacao) < tamanho_populacao:
+            if random.random() < probabilidade_mutacao:
+                m = random.randint(0, numero_elitismo)
+                populacao.append(mutacao(dominio, passo, individuos_ordenados[m]))
+            else:
+                c1 = random.randint(0, numero_elitismo)
+                c2 = random.randint(0, numero_elitismo)
+                populacao.append(cruzamento(dominio, individuos_ordenados[c1], individuos_ordenados[c2]))
+    
+    return custos[0][1]
+
+solucao_genetico = genetico(dominio, funcao_custo)
+custo_genetico = funcao_custo(solucao_genetico)
+print('------------------------')
+print('SOLUCAO GENETICA')
+print('------------------------')
+imprimir_solucao(solucao_genetico)
