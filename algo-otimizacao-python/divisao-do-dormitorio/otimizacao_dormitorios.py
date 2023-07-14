5a736d20c8bb7cabaf2dae107ca68dc4f746cfbd
diogomarin <70486076+diogomarin@users.noreply.github.com>
2023-07-14 16:29:20 -0300

    implementacao da subida da encosta

--- algo-otimizacao-python/divisao-do-dormitorio/otimizacao_dormitorios.py
+++ algo-otimizacao-python/divisao-do-dormitorio/otimizacao_dormitorios.py
@@ -69,5 +69,34 @@ print('SOLUCAO APENAS ALEATORIA')
 print('------------------------')
 imprimir_solucao(solucao_randomica)
 
+def subida_encosta(dominio, funcao_custo):
+    solucao = [random.randint(dominio[i][0], dominio[i][1]) for i in range(len(dominio))]
+    while True:
+        vizinhos = []
 
+        for i in range(len(dominio)):
+            if solucao[i] > dominio[i][0]:
+                 if solucao [i] != dominio[i][1]:
+                     vizinhos.append(solucao[0:i] + [solucao[i] + 1] + solucao[i + 1:])
+            if solucao [i] < dominio[i][1]:
+                if solucao[i] != dominio[i][0]:
+                  vizinhos.append(solucao[0:i] + [solucao[i] - 1] + solucao [i + 1:])
+        atual = funcao_custo(solucao)
+        melhor = atual
+        for i in range(len(vizinhos)):
+            custo = funcao_custo(vizinhos[i])
+            if custo < melhor:
+                melhor = custo
+                solucao = vizinhos[i]
+        
+        if melhor == atual:
+            break
+    return solucao
+
+solucao_subida_encosta = subida_encosta(dominio, funcao_custo)
+custo_subida_encosta = funcao_custo(solucao_subida_encosta)
+print('------------------------')
+print('SOLUCAO HILL CLIMB')
+print('------------------------')
+imprimir_solucao(solucao_subida_encosta)
 
