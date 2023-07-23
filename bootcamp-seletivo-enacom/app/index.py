b19c51672f2f90b06c8bb7671314486aab3dc336
diogomarin <70486076+diogomarin@users.noreply.github.com>
2023-07-23 04:13:51 -0300

    ambiente conda e arquivos base do app

---
+++ bootcamp-seletivo-enacom/app/index.py
@@ -0,0 +1,14 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from app import *
+
+# ===== LAYOUT ====== #
+content = html.Div(id="page-content")
+
+
+app.layout
