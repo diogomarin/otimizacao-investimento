ba6b4d60a95746cb57aa294e3705db16248a43dc
diogomarin <70486076+diogomarin@users.noreply.github.com>
2023-07-24 09:26:04 -0300

    construcao do layout base

--- bootcamp-seletivo-enacom/app/index.py
+++ bootcamp-seletivo-enacom/app/index.py
@@ -6,15 +6,32 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
+from components import sidebar, dashboards, investimentos
 
 # ===== LAYOUT ====== #
 content = html.Div(id="page-content")
 
+app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id="url"),
+            sidebar.layout
+        ], md=2, style={'background': 'red', 'height': '1080px'}),
 
-app.layout = dbc.Container(chilren=[
-
+        dbc.Col([
+            content
+        ], md=10, style={'background': 'blue', 'height': '1080px'}),
+    ])
 
 ], fluid=True,)
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/" or pathname == "/dashboards":
+        return dashboards.layout
+    
+    if pathname == "/investimentos":
+        return investimentos.layout
 
 
 if __name__ == '__main__':
